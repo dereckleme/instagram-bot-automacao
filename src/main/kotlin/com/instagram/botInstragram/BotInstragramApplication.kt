47e81d54918427a4e75adc523cb3eed869c38d88
Dereck Leme <dereckvicentin@gmail.com>
2022-12-20 21:54:24 -0300

    first commit

---
+++ src/main/kotlin/com/instagram/botInstragram/BotInstragramApplication.kt
@@ -0,0 +1,11 @@
+package com.instagram.botInstragram
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BotInstragramApplication
+
+fun main(args: Array<String>) {
+	runApplication<BotInstragramApplication>(*args)
+}
---
+++ src/test/kotlin/com/instagram/botInstragram/BotInstragramApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.instagram.botInstragram
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BotInstragramApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
